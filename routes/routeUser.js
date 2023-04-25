d2ec7230b882cfbe9f24e6a93ca3fe2a3f631ca8
ming wu <a85062@pchome.com.tw>
2023-04-25 20:49:53 +0800

    跨域表頭設定

--- routes/routeUser.js
+++ routes/routeUser.js
@@ -8,7 +8,7 @@ const middlewareAuth = require('@/middlewares/middlewareAuth')
 const router = express.Router()
 
 const controllerUser = require('@/controllers/controllerUser')
-router.options('/signup', serviceError.asyncError(async (req, res, next) => {
+router.options('/', serviceError.asyncError(async (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'content-type')
   res.end()
@@ -18,10 +18,15 @@ router.post('/signin', serviceError.asyncError(async (req, res, next) => {
   serviceResponse.success(res, result)
 }))
 
-router.post('/signup', serviceError.asyncError(async (req, res, next) => {
-  const result = await controllerUser.signup(req, res, next)
-  serviceResponse.success(res, result)
+router.options('/signup', serviceError.asyncError(async (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'content-type')
+  res.end()
 }))
+  .post('/signup', serviceError.asyncError(async (req, res, next) => {
+    const result = await controllerUser.signup(req, res, next)
+    serviceResponse.success(res, result)
+  }))
 
 router.post('/updateUser', middlewareAuth.loginAuth, serviceError.asyncError(async (req, res, next) => {
   const result = await controllerUser.updateUser(req, res, next)
